869c3a54c9a5253e479919adcb263c9f6e3c575f
Rikuoh <mail@riq0h.jp>
2025-08-28 22:20:15 +0900

    未収載がDMとして処理される不具合を修正

--- app/controllers/concerns/activity_pub_visibility_helper.rb
+++ app/controllers/concerns/activity_pub_visibility_helper.rb
@@ -9,10 +9,27 @@ module ActivityPubVisibilityHelper
     to = Array(object_data['to'])
     cc = Array(object_data['cc'])
 
-    return 'public' if to.include?('https://www.w3.org/ns/activitystreams#Public')
-    return 'unlisted' if cc.include?('https://www.w3.org/ns/activitystreams#Public')
-    return 'direct' if to.any? && cc.empty?
+    public_collection = 'https://www.w3.org/ns/activitystreams#Public'
 
+    # Public: as:Public が to に含まれる
+    return 'public' if to.include?(public_collection)
+
+    # Unlisted: as:Public が cc に含まれる
+    return 'unlisted' if cc.include?(public_collection)
+
+    # Direct: 特定のアクターのみが対象で、mentions がある
+    mentions = extract_mentions_from_tags(object_data['tag'])
+    return 'direct' if mentions.any? && (to + cc).all? { |recipient| mentions.include?(recipient) || recipient.include?('/followers') }
+
+    # Private: フォロワーのみ（as:Public が含まれない）
     'private'
+  end
+
+  def extract_mentions_from_tags(tags)
+    return [] unless tags.is_a?(Array)
+
+    tags.select { |tag| tag['type'] == 'Mention' }
+        .pluck('href')
+        .compact
   end
 end
