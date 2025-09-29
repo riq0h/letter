5dd9eb8e90b9b8057f9297401d361ace0724c3fd
Rikuoh <mail@riq0h.jp>
2025-09-29 22:28:37 +0900

    マウントポイントを変更

--- db/migrate/20250912102725_add_access_token_id_to_web_push_subscriptions.rb
+++ db/migrate/20250912102725_add_access_token_id_to_web_push_subscriptions.rb
@@ -1,7 +1,9 @@
 class AddAccessTokenIdToWebPushSubscriptions < ActiveRecord::Migration[8.0]
   def change
-    add_column :web_push_subscriptions, :access_token_id, :integer
-    add_index :web_push_subscriptions, :access_token_id
-    add_foreign_key :web_push_subscriptions, :oauth_access_tokens, column: :access_token_id
+    unless column_exists?(:web_push_subscriptions, :access_token_id)
+      add_column :web_push_subscriptions, :access_token_id, :integer
+      add_index :web_push_subscriptions, :access_token_id
+      add_foreign_key :web_push_subscriptions, :oauth_access_tokens, column: :access_token_id
+    end
   end
 end
