edd386b44e1d8dd10fa2b6a6df35b1f0af0c1039
Rikuoh <mail@riq0h.jp>
2026-02-17 17:38:05 +0900

    トークンの有効期限を事実上無制限に設定

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -97,12 +97,9 @@ Doorkeeper.configure do
   # authorization_code_expires_in 10.minutes
 
   # Access token expiration time (default: 2 hours).
-  # If you set this to `nil` Doorkeeper will not expire the token and omit expires_in in response.
-  # It is RECOMMENDED to set expiration time explicitly.
-  # Prefer access_token_expires_in 100.years or similar,
-  # which would be functionally equivalent and avoid the risk of unexpected behavior by callers.
-  #
-  # access_token_expires_in 2.hours
+  # Mastodon互換: クライアントはリフレッシュトークンフローを実装していないため、
+  # トークンを事実上無期限にする
+  access_token_expires_in 100.years
 
   # Assign custom TTL for access tokens. Will be used instead of access_token_expires_in
   # option if defined. In case the block returns `nil` value Doorkeeper fallbacks to
