ad289c14b5988264285fa4b6a4ed13efac60f48f
Rikuoh <mail@riq0h.jp>
2026-02-22 10:01:21 +0900

    sitemapsにアクセスされた時、プレーンテキストで404を返す

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,21 +4,21 @@ class ErrorsController < ApplicationController
   def not_found
     respond_to do |format|
       format.json { render json: { error: 'Not Found' }, status: :not_found }
-      format.all { render status: :not_found }
+      format.all { render plain: 'Not Found', status: :not_found }
     end
   end
 
   def unprocessable_entity
     respond_to do |format|
       format.json { render json: { error: 'Unprocessable Entity' }, status: :unprocessable_content }
-      format.all { render status: :unprocessable_content }
+      format.all { render plain: 'Unprocessable Entity', status: :unprocessable_content }
     end
   end
 
   def internal_server_error
     respond_to do |format|
       format.json { render json: { error: 'Internal Server Error' }, status: :internal_server_error }
-      format.all { render status: :internal_server_error }
+      format.all { render plain: 'Internal Server Error', status: :internal_server_error }
     end
   end
 
