29837b8db44588fbe22546346b0198bdd5109a44
辻谷陸王 <r.tsujitani@e-gfs.co.jp>
2026-03-16 13:25:02 +0900

    メディアプロキシの実装

--- app/jobs/remote_media_download_job.rb
+++ app/jobs/remote_media_download_job.rb
@@ -7,9 +7,10 @@ class RemoteMediaDownloadJob < ApplicationJob
 
   queue_as :default
 
-  MAX_VIDEO_SIZE = 100.megabytes
-  MAX_IMAGE_SIZE = 10.megabytes
+  MAX_VIDEO_SIZE = 300.megabytes
+  MAX_IMAGE_SIZE = 30.megabytes
   DOWNLOAD_TIMEOUT = 60
+  MAX_REDIRECTS = 5
 
   def perform(media_attachment_id)
     media = MediaAttachment.find_by(id: media_attachment_id)
@@ -61,7 +62,11 @@ class RemoteMediaDownloadJob < ApplicationJob
 
     tempfile = Tempfile.new(['remote_media', '.bin'], binmode: true)
 
-    stream_response(uri, tempfile, max_size)
+    # リダイレクトを追従してダウンロード
+    final_uri = follow_redirects(uri)
+    return nil unless final_uri
+
+    stream_response(final_uri, tempfile, max_size)
 
     tempfile.rewind
     tempfile
@@ -69,6 +74,29 @@ class RemoteMediaDownloadJob < ApplicationJob
     tempfile&.close!
     Rails.logger.warn "RemoteMediaDownloadJob: download failed: #{e.message}"
     nil
+  end
+
+  def follow_redirects(uri, limit = MAX_REDIRECTS)
+    raise 'too many redirects' if limit <= 0
+
+    Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https',
+                                        open_timeout: 10, read_timeout: 10) do |http|
+      response = http.head(uri)
+
+      case response
+      when Net::HTTPSuccess
+        uri
+      when Net::HTTPRedirection
+        redirect_uri = URI.parse(response['location'])
+        # 相対URLの場合は元のホストを使う
+        redirect_uri = URI.join("#{uri.scheme}://#{uri.host}", response['location']) unless redirect_uri.host
+        return nil unless validate_url_for_ssrf!(redirect_uri.to_s)
+
+        follow_redirects(redirect_uri, limit - 1)
+      else
+        raise "HTTP #{response.code}"
+      end
+    end
   end
 
   def stream_response(uri, tempfile, max_size)
@@ -81,17 +109,12 @@ class RemoteMediaDownloadJob < ApplicationJob
       request['User-Agent'] = InstanceConfig.user_agent
 
       http.request(request) do |response|
-        unless response.is_a?(Net::HTTPSuccess)
-          tempfile.close!
-          raise "HTTP #{response.code}"
-        end
+        raise "HTTP #{response.code}" unless response.is_a?(Net::HTTPSuccess)
 
         response.read_body do |chunk|
           downloaded_size += chunk.bytesize
-          if downloaded_size > max_size
-            tempfile.close!
-            raise "file too large (>#{max_size} bytes)"
-          end
+          raise "file too large (>#{max_size} bytes)" if downloaded_size > max_size
+
           tempfile.write(chunk)
         end
       end
