ebf1d73a0362a667e3838f5cd311ffcad7322d53
辻谷陸王 <r.tsujitani@e-gfs.co.jp>
2026-04-01 14:28:27 +0900

    SQLiteの競合対策

---
+++ config/initializers/sqlite_busy_timeout.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# SQLiteのbusy_timeoutを設定
+# PRAGMAやdatabase.ymlのtimeoutでは反映されないため、
+# コネクション確立時にRubyレベルで直接設定する
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::ConnectionAdapters::SQLite3Adapter.prepend(Module.new do
+    def configure_connection
+      super
+      raw_connection.busy_timeout(5000)
+    end
+  end)
+end
