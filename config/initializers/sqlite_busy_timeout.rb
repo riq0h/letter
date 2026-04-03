354bd2d2dc975aa812ee6f3b90cc8e06ce0d769c
辻谷陸王 <r.tsujitani@e-gfs.co.jp>
2026-04-03 10:25:33 +0900

    mmapサイズを拡大

--- config/initializers/sqlite_busy_timeout.rb
+++ config/initializers/sqlite_busy_timeout.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-# SQLiteのbusy_timeoutを設定
-# database.ymlのtimeoutとは別に、接続レベルで明示的に設定する
+# SQLiteの接続レベル設定
+# database.ymlのpragmaでは反映されない設定を明示的に適用する
 ActiveSupport.on_load(:active_record) do
   ActiveRecord::ConnectionAdapters::SQLite3Adapter.prepend(Module.new do
     def configure_connection
       super
 
-      raw_connection.busy_timeout(15_000) # 15秒
+      raw_connection.busy_timeout(15_000)
+      raw_connection.execute('PRAGMA mmap_size=2147483648') # 2GB
     end
   end)
 end
